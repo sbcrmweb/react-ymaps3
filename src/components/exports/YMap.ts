eae331a422561a4003486e054c6b9c46ad7b8e97
sbcrmweb <dev-sberbp@sberbs.ru>
2024-02-14 17:43:51 +0300

    feat: testing another way of component export

--- src/components/exports/YMap.ts
+++ src/components/exports/YMap.ts
@@ -1,12 +1,19 @@
 import { YMap as YMapHandle } from '@yandex/ymaps3-types';
 import { YMap as YMapComponent } from '@yandex/ymaps3-types/react';
 import utils from '../../utils';
+import { Modify } from '../../utils/typescript';
 
-export interface YMapProps
-  extends React.ComponentProps<typeof YMapComponent> {}
+export type YMapProps = Modify<
+  React.ComponentPropsWithRef<typeof YMapComponent>,
+  {
+    key?: React.Key;
+  }
+>;
 
 const YMap = utils.createElement<YMapHandle, YMapProps>(
   'YMap',
-) as typeof YMapComponent;
+) as React.ForwardRefExoticComponent<YMapProps>;
 
 export default YMap;
+
+export { YMap as YMapHandle } from '@yandex/ymaps3-types';
