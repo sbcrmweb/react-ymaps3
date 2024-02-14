cb99a72e2f3ffcb08fca13a575309133ca6b1efe
sbcrmweb <dev-sberbp@sberbs.ru>
2024-02-14 14:22:55 +0300

    test with cjs

--- tsup.config.ts
+++ tsup.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   bundle: false,
   treeshake: true,
   splitting: true,
-  format: ['esm'],
+  format: ['esm', 'cjs'],
   entry: ['src/index.ts', 'src/**/*.ts', 'src/**/*.tsx'],
   sourcemap: true,
   clean: false,
