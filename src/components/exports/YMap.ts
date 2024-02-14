5e83ad04d29d49afd6fc6aa9e3cf1bfb73063ee1
sbcrmweb <dev-sberbp@sberbs.ru>
2024-02-14 18:32:13 +0300

    test: types react issues

--- src/components/exports/YMap.ts
+++ src/components/exports/YMap.ts
@@ -2,7 +2,7 @@ import { YMap as YMapHandle } from '@yandex/ymaps3-types';
 import { YMap as YMapComponent } from '@yandex/ymaps3-types/react';
 import utils from '../../utils';
 
-export type YMapProps = React.ComponentPropsWithRef<
+export type YMapProps = React.ComponentProps<
   typeof YMapComponent
 >;
 
@@ -12,4 +12,4 @@ const YMap = utils.createElement<YMapHandle, YMapProps>(
 
 export default YMap;
 
-export { YMap as YMapHandle } from '@yandex/ymaps3-types';
+//export { YMap as YMapHandle } from '@yandex/ymaps3-types';
